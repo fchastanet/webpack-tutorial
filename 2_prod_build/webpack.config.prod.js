e509dfe1a95f864fd544c469f27147588c249024
Francois Chastanet <fchastanet@wiley.com>
2021-03-25 18:56:49 +0100

    step2 without minification, with bundle-analysis

--- 2_prod_build/webpack.config.prod.js
+++ 2_prod_build/webpack.config.prod.js
@@ -3,16 +3,8 @@
 const { merge } = require('webpack-merge')
 const baseConfig = require('./webpack.config')
 
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-
 module.exports = merge(baseConfig, {
   mode: 'production',
-  optimization: {
-    minimizer: [
-      new CssMinimizerPlugin(),
-      '...' // use default TerserPlugin configuration
-    ]
-  },
   devServer: {
     open: true,
     contentBase: './dist',
